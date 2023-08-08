6918a67d3784220fd47ad130c5fcd4d5bcc5e041
santhosh-sfw <santhosh@sfwtechnologies.com>
2023-08-08 11:16:09 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LyHammerGestureConfig, LY_THEME_NAME, StyleRenderer, LyTheme2 } from '@alyle/ui';
+import { MinimaLight } from '@alyle/ui/themes/minima';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { LyThemeModule, LY_THEME} from '@alyle/ui'
+
+export const createTranslateHttpLoader = (http: HttpClient) => {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HammerModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    NgIdleKeepaliveModule.forRoot(),
+    ModalModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateHttpLoader),
+          deps: [HttpClient]
+      }
+  }),
+  LyThemeModule.setTheme('minima-light'),
+  AppRoutingModule
+  ],
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: LyHammerGestureConfig },
+    StyleRenderer,
+    LyTheme2,
+    { provide: LY_THEME_NAME, useValue: 'minima-light' },
+    { provide: LY_THEME, useClass: MinimaLight, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
